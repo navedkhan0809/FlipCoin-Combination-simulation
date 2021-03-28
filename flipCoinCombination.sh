ee3967632fe46323ec47b0857e195b47c0c0d212
Naved Khan <navedkhan0809@gmail.com>
2021-03-28 10:55:34 +0530

    Added: Singlet combination with percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,11 +1,35 @@
 #!/bin/bash -x
 
-toss=$((RANDOM%2))
-HEADS=1
+i=0
+j=0
+k=0
 
-if [ $toss -eq $HEADS ]
-then
-   echo "head"
-else
-   echo "tail"
-fi
+echo -e "Enter Number of times of a Coin flips=\c"
+read f
+
+while [ $i -lt $f ]
+do
+	toss=$((RANDOM%2))
+	HEADS=1
+
+	if [ $toss -eq $HEADS ]
+	then
+   	j=$(($j+1))
+	else
+   	k=$(($k+1))
+	fi
+i=$(($i + 1))
+done
+
+echo "You won HEADS $j times"
+echo "You won TAILS $k times"
+
+declare -A flip
+for ((p=1; p<=$j; p++))
+do
+	flip[$p]="HEADS"
+done
+echo ${flip[@]}
+
+perc=`echo "scale=2;($j/$f)*100"|bc`
+echo "percentage of heads = $perc %"
